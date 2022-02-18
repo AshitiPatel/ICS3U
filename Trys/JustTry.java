cf9e8851bae5e3c6c5eb8c9a0e451ee78691d38b
Ashiti Patel <79819794+AshitiPatel@users.noreply.github.com>
2022-02-18 18:14:40 -0500

    Add files via upload

---
+++ Trys/JustTry.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ashii
+ *
+ */
+public class JustTry {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		int[] abcd = { 4, 5, 6, 7, 8};
+//		System.out.println(abcd[abcd.length - 1]);
+//		
+
+		
+//		int numbers[] = null;
+//		
+//		for(int i = 0; i < (numbers.length - 1); i++) {
+//			numbers[i] = 1;
+//		}
+//		
+		double[] averages; 
+		averages = new double [7];
+		
+		averages[0] = 75;
+		averages[1] = 78.5;
+		averages[2] = 72.5;
+		averages[3] = 77;
+		averages[4] = 90;
+		averages[5] = 70;
+		averages[6] = 89;
+		
+//		double temp;
+//
+//		temp = averages [0];
+//		averages [0] = averages [5];
+//		averages [3] = averages [3];
+//		averages [5] = averages [2];
+//		averages [1] = averages [5];
+//		averages [3] = temp;
+//		
+//		System.out.println(averages[0]);
+//		System.out.println(averages[1]);
+//		System.out.println(averages[2]);
+//		System.out.println(averages[3]);
+//		System.out.println(averages[4]);
+//		System.out.println(averages[5]);
+//		System.out.println(averages[6]);
+		 
+		String[] courses;
+		courses = new String [7];
+		
+		courses[0] = "Comp. Sci.";
+		courses[1] = "Math";
+		courses[2] = "Physics";
+		courses[3] = "Accounting";
+		courses[4] = "Auto";
+		courses[5] = "English";
+		courses[6] = "French";
+		
+		System.out.println(courses[0]);
+		//System.out.println(averages[7]);
+		
+		System.out.println(((courses.length - 1)/2));
+		
+		
+	}
+
+}
